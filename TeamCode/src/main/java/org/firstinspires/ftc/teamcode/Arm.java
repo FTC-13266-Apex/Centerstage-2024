a743039e6e628a9d70e47fd5b946a155df243e0e
Helen <Hlebvaldivia012@gmail.com>
2023-11-11 15:51:15 -0600

    moved into subsystems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -1,24 +1,34 @@
-package org.firstinspires.ftc.teamcode.Tests;
+package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 @TeleOp
-public class Arm extends LinearOpMode {
-    @Override
-    public void runOpMode() throws InterruptedException {
-        // Position of the arm when it's lifted
-        int armUpPosition = 1000;
+public class Arm  {
+   private final DcMotor armMotor;
+    private final Gamepad gamepad2;
+    private final Telemetry telemetry;
+    private final Servo tClaw;
+    // Position of the arm when it's lifted
+    int armUpPosition = -700;
 
-        // Position of the arm when it's down
-        int armDownPosition = 0;
+    // Position of the arm when it's down
+    int armDownPosition = 0;
+    public Arm(OpMode opMode) {
+        HardwareMap hardwareMap = opMode.hardwareMap;
+        tClaw = (Servo) hardwareMap.get("tClaw");
+        tClaw.setDirection(Servo.Direction.FORWARD);
 
-        // Find a motor in the hardware map named "Arm Motor"
-        DcMotor armMotor = hardwareMap.dcMotor.get("Arm Motor");
+        // Find a motor in the hardware map named "Arm Motor
+        telemetry = opMode.telemetry;
+        armMotor = hardwareMap.dcMotor.get("Arm Motor");
 
 
         // Reset the motor encoder so that it reads zero ticks
@@ -28,59 +38,47 @@ public class Arm extends LinearOpMode {
         armMotor.setTargetPosition(armDownPosition);
         armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        waitForStart();
+       // waitForStart();
+        gamepad2 = opMode.gamepad2;
 
-        while (opModeIsActive()) {
-            // If the A button is pressed, raise the arm
-            if (gamepad1.a) {
-                armMotor.setTargetPosition(armUpPosition);
-                armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                armMotor.setPower(0.5);
-            }
 
-            // If the B button is pressed, lower the arm
-            if (gamepad1.b) {
-                armMotor.setTargetPosition(armDownPosition);
-                armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                armMotor.setPower(0.5);
-            }
 
-            // Get the current position of the armMotor
-            double position = armMotor.getCurrentPosition();
+            
 
-            // Get the target position of the armMotor
-            double desiredPosition = armMotor.getTargetPosition();
 
-            // Show the position of the armMotor on telemetry
-            telemetry.addData("Encoder Position", position);
-
-            // Show the target position of the armMotor on telemetry
-            telemetry.addData("Desired Position", desiredPosition);
-
-            telemetry.update();
-        }
     }
-    public class Claw {
-        private Servo lClaw, rClaw;
-        private final Gamepad gamepad2;
-        public Claw(HardwareMap hardwareMap, Gamepad gamepad2) {
-            lClaw = (Servo) hardwareMap.get("lClaw");
-            rClaw = (Servo) hardwareMap.get("rClaw");
-            this.gamepad2 = gamepad2;
+    void teleOp(){
+        //Arm(HardwareMap, hardwareMap, Gamepad ,gamepad2) {
+        // If the A button is pressed, raise the arm
+        if (gamepad2.a) {
+            tClaw.setPosition(0.59);
+            armMotor.setTargetPosition(armUpPosition);
+            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            armMotor.setPower(0.5);
+        }
 
-            lClaw.setDirection(Servo.Direction.REVERSE);
-            rClaw.setDirection(Servo.Direction.FORWARD);
-            clawServo(0.42,0.42);
+        // If the B button is pressed, lower the arm
+        if (gamepad2.b) {
+            tClaw.setPosition(0.4);
+            armMotor.setTargetPosition(armDownPosition);
+            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            armMotor.setPower(0.5);
         }
-        public void teleOp() {
-            //TODO Change position left trigger outtake
-            if (gamepad2.dpad_down) clawServo(0.59, 0.59);
-            else if (gamepad2.dpad_left) clawServo(0.4,0.4);
-        }
-        public void clawServo(double setPositionRight, double setPositionLeft) {
-            rClaw.setPosition(setPositionRight);
-            lClaw.setPosition(setPositionLeft);
-        }
+
+        // Get the current position of the armMotor
+        double position = armMotor.getCurrentPosition();
+
+        // Get the target position of the armMotor
+        double desiredPosition = armMotor.getTargetPosition();
+
+        // Show the position of the armMotor on telemetry
+        telemetry.addData("Encoder Position", position);
+
+        // Show the target position of the armMotor on telemetry
+        telemetry.addData("Desired Position", desiredPosition);
+
     }
+
+
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -1,31 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-    public class Claw {
-        private Servo oClaw, tClaw;
-        private final Gamepad gamepad2;
-        public Claw(HardwareMap hardwareMap, Gamepad gamepad2) {
-            oClaw = (Servo) hardwareMap.get("oClaw");
-            tClaw = (Servo) hardwareMap.get("tClaw");
-            this.gamepad2 = gamepad2;
-            oClaw.setDirection(Servo.Direction.REVERSE);
-            tClaw.setDirection(Servo.Direction.FORWARD);
-            clawServo(0.42,0.42);
-        }
-        public void teleOp() {
-            //TODO Change position left trigger outtake
-            if (gamepad2.dpad_up) clawServo(0.59, 0.59);
-            else if (gamepad2.dpad_right) clawServo(0.4,0.4);
-        }
-        public void clawServo(double setPositionOpen, double setPositionTurn) {
-            oClaw.setPosition(setPositionOpen);
-            tClaw.setPosition(setPositionTurn);
-        }
+public class Claw {
+    private final Servo oClaw;
+    private final Gamepad gamepad2;
+    public Claw(OpMode opMode) {
+        HardwareMap hardwareMap = opMode.hardwareMap;
+        gamepad2 = opMode.gamepad2;
+        oClaw = (Servo) hardwareMap.get("oClaw");
 
 
+        oClaw.setDirection(Servo.Direction.REVERSE);
+        oClaw.setPosition(0.4);
     }
+    public void teleOp() {
+        //TODO Change position left trigger outtake
+        if (gamepad2.dpad_up) oClaw.setPosition(0.59);
+        else if (gamepad2.dpad_right)oClaw.setPosition (0.4);
+    }
+
+
+}
 
