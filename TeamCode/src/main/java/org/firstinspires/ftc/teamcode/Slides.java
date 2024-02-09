a065bce5880fc87ff54809c72c999c9694bffaff
CRAM <grsmystic@gmail.com>
2024-02-08 18:10:51 -0800

    changable slide positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Slides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Slides.java
@@ -22,6 +22,8 @@ public class Slides  {
     int low = 1;
     int mid = 2;
     int high = 3;
+    int targetPosition = intake;
+
     public Slides (OpMode opMode) {
         HardwareMap hardwareMap = opMode.hardwareMap;
 
@@ -38,58 +40,53 @@ public class Slides  {
         // Sets the starting position of the arm to the down position
         leftSlide.setDirection(DcMotorSimple.Direction.FORWARD);
         rightSlide.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftSlide.setTargetPosition(intake);
+        rightSlide.setTargetPosition(intake);
         leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         leftSlide.setPower(0.5);
         rightSlide.setPower(0.5);
-        leftSlide.setTargetPosition(intake);
-        rightSlide.setTargetPosition(intake);
         leftSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        // waitForStart();
         gamepad2 = opMode.gamepad2;
-
-
     }
 
     void teleOp(){
-
-
-
-
         // Get the current position of the armMotor
-        double leftposition = leftSlide.getCurrentPosition();
-        double rightposition = rightSlide.getCurrentPosition();
+        double leftPosition = leftSlide.getCurrentPosition();
+        double rightPosition = rightSlide.getCurrentPosition();
 
         // Get the target position of the armMotor
-        double lefttarget = leftSlide.getTargetPosition();
-        double righttarget = rightSlide.getTargetPosition();
+        double leftTarget = leftSlide.getTargetPosition();
+        double rightTarget = rightSlide.getTargetPosition();
 
         // Show the position of the armMotor on telemetry
-        telemetry.addData("Encoder Position", leftposition);
-        telemetry.addData("Encoder Position", rightposition);
+        telemetry.addData("Encoder Position", leftPosition);
+        telemetry.addData("Encoder Position", rightPosition);
 
         // Show the target position of the armMotor on telemetry
-        telemetry.addData("Desired Position", lefttarget);
-        telemetry.addData("Desired Position", righttarget);
+        telemetry.addData("Desired Position", leftTarget);
+        telemetry.addData("Desired Position", rightTarget);
 
 
-        if (gamepad2.a){
-            leftSlide.setTargetPosition(intake);
-            rightSlide.setTargetPosition(intake);
+        leftSlide.setTargetPosition(targetPosition);
+        rightSlide.setTargetPosition(targetPosition);
+
+        if (gamepad2.a) {
+            targetPosition= intake;
         }
-        if (gamepad2.b){
-            leftSlide.setTargetPosition(low);
-            rightSlide.setTargetPosition(low);
+        if (gamepad2.b) {
+            targetPosition = low;
         }
-        if (gamepad2.y){
-            leftSlide.setTargetPosition(mid);
-            leftSlide.setTargetPosition(mid);
+        if (gamepad2.y) {
+            targetPosition = mid;
         }
-        if (gamepad2.x){
-            leftSlide.setTargetPosition(high);
-            rightSlide.setTargetPosition(high);
+        if (gamepad2.x) {
+            targetPosition = high;
+        }
+        if (gamepad2.right_stick_y > 0.1 || gamepad2.right_stick_y < -0.1) {
+            targetPosition += (int) gamepad2.right_stick_y;
         }
     }
 }
