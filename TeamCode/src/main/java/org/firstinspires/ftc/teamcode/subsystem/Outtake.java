652d376c9c8feb0f6539eec43bcb6cc87b7686d1
CRAM <grsmystic@gmail.com>
2024-02-19 15:16:14 -0600

    Added button bindings

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Outtake.java
@@ -5,24 +5,34 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
     @Config
     public class Outtake {
         private final Gamepad gamepad1;
+        private final Gamepad gamepad2;
         private final Telemetry telemetry;
 
+        private final Servo leftServo;
+        private final Servo rightServo;
         private final CRServo intakeServo;
 
         public Outtake (OpMode opMode) {
             HardwareMap hardwareMap = opMode.hardwareMap;
 
             intakeServo = hardwareMap.crservo.get("intakeServo");
+            leftServo = hardwareMap.servo.get("leftServo");
+            rightServo = hardwareMap.servo.get("rightServo");
 
             telemetry = opMode.telemetry;
-
             gamepad1 = opMode.gamepad1;
+            gamepad2 = opMode.gamepad2;
+
+            leftServo.setPosition(1);
+            rightServo.setPosition(0);
+
         }
 
         public void teleOp() {
@@ -37,6 +47,13 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
             }
             else {
                 intakeServo.setPower(0);
+            }
+            if (gamepad2.a){
+                leftServo.setPosition(1);
+                rightServo.setPosition(0);
+            } else if (gamepad2.b || gamepad2.x || gamepad2.y) {
+                leftServo.setPosition(0.5);
+                rightServo.setPosition(0.5);
             }
         }
     }
